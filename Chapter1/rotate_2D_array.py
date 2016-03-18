95aa223b8f831882c00747291bc40a51c5cbb6a3
CQ <chunqiu.wang@outlook.com>
2016-03-17 22:50:23 -0500

    rotate_2D_array

---
+++ Chapter1/rotate_2D_array.py
@@ -0,0 +1,11 @@
+'''
+Created on Mar 17, 2016
+Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, 
+write a method to rotate the image by 90 degrees. Can you do this in place?
+@author: chunq
+'''
+
+def rotate2DArray():
+    
+if __name__ == "__main__":
+    print(rotate2DArray())
