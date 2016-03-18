3471aafada3be94383e0ed4cbe36d9cf525cbadb
CQ <chunqiu.wang@outlook.com>
2016-03-17 22:21:10 -0500

    removeDuplicateChars

--- Chapter1/remove_duplicate_chars_in_string.py
+++ Chapter1/remove_duplicate_chars_in_string.py
@@ -10,6 +10,40 @@ Created on Mar 16, 2016
 @author: chunq
 '''
 
-def removeDuplicateChars(stringToRemove):
+def removeDuplicateCharsWithArray(stringToRemove):
+    charAry = [None] * 128
+    newString = ""
+    for char in stringToRemove:
+        if charAry[ord(char)]:
+            continue
+        else:
+            newString = newString + char
+            charAry[ord(char)] = True
+    return newString
+            
+def removeDuplicateCharsWithoutArray(stringToRemove):
+    newString = ""
+    for index1 in range(0, len(stringToRemove)):
+        foundDuplicate = False
+        for index2 in range(index1 + 1, len(stringToRemove)):
+            if stringToRemove[index1] == stringToRemove[index2]:
+                foundDuplicate = True
+                break
+        if foundDuplicate:
+            continue
+        else:
+            newString = newString + stringToRemove[index1]
+    return newString
+    
+if __name__ == "__main__":
+    print(removeDuplicateCharsWithArray("sstringToCheck"))
+    print(removeDuplicateCharsWithArray("aaaa"))
+    print(removeDuplicateCharsWithArray("abba"))
+    print(removeDuplicateCharsWithArray(""))
+    print(removeDuplicateCharsWithoutArray("sstringToCheck"))
+    print(removeDuplicateCharsWithoutArray("aaaa"))
+    print(removeDuplicateCharsWithoutArray("aabb"))
+    print(removeDuplicateCharsWithoutArray(""))
+        
     
     
