cf63fc373e5a516626c229b7cfbde6b0cda0213c
CQ <chunqiu.wang@outlook.com>
2016-03-29 16:22:30 -0500

    rotate 2D array

--- Chapter1/rotate_2D_array.py
+++ Chapter1/rotate_2D_array.py
@@ -5,7 +5,44 @@ write a method to rotate the image by 90 degrees. Can you do this in place?
 @author: chunq
 '''
 
-def rotate2DArray():
-    
+def rotate2DArray(arrayToBeRotated, isCounterClockwise):
+	totalLayers = len(arrayToBeRotated[0])
+	for layer in range(0, int(totalLayers/2)):
+		rotateOneLayer(arrayToBeRotated, layer, isCounterClockwise)
+	return arrayToBeRotated
+
+def rotateOneLayer(arrayToBeRotated, layer, isCounterClockwise):
+	for position in range(layer, len(arrayToBeRotated[0]) - layer - 1):
+		moveOneElement(arrayToBeRotated, layer, position, isCounterClockwise)
+	return
+
+def moveOneElement(arrayToBeRotated, start, position, isCounterClockwise):
+	end = len(arrayToBeRotated[0]) - start - 1
+	offset = position - start
+
+	if isCounterClockwise:
+		rotateOneElementClockwise(arrayToBeRotated, start, end, offset)
+	else:
+		rotateOneElementCounterClockwise(arrayToBeRotated, start, end, offset)
+	return
+	    
+def rotateOneElementClockwise(arrayToBeRotated, start, end, offset):
+	backUp = arrayToBeRotated[start][start + offset]
+	arrayToBeRotated[start][start + offset] = arrayToBeRotated[end - offset][start]
+	arrayToBeRotated[end - offset][start] = arrayToBeRotated[end][end - offset]
+	arrayToBeRotated[end][end - offset] = arrayToBeRotated[start + offset][end]
+	arrayToBeRotated[start + offset][end] = backUp
+
+def rotateOneElementCounterClockwise(arrayToBeRotated, start, end, offset):
+	backUp = arrayToBeRotated[start][start + offset]
+	arrayToBeRotated[start][start + offset] = arrayToBeRotated[start + offset][end]
+	arrayToBeRotated[start + offset][end] = arrayToBeRotated[end][end - offset]
+	arrayToBeRotated[end][end - offset] = arrayToBeRotated[end - offset][start]
+	arrayToBeRotated[end - offset][start] = backUp
+	return
+
 if __name__ == "__main__":
-    print(rotate2DArray())
+	print(rotate2DArray([[1,1,1],[2,2,2],[3,3,3]], False))
+	print(rotate2DArray([[1,1,1],[2,2,2],[3,3,3]], True))
+	print(rotate2DArray([[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]], True))
+	print(rotate2DArray([[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]], False))
