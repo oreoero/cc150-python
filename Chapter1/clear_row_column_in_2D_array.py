a0ce10195f6a884cc8df32092a3b347150541eb7
CQ <chunqiu.wang@outlook.com>
2016-03-29 17:07:22 -0500

    check zero in matrix

---
+++ Chapter1/clear_row_column_in_2D_array.py
@@ -0,0 +1,27 @@
+'''
+Created on Mar 29, 2016
+1.7 Write an algorithm such that if an element in an MxN matrix is 0, 
+its entire row and column is set to 0.
+@author: chunq
+'''
+
+def clearRowColumnInMatrix(matrix):
+    rowHelper = [False] * len(matrix[0])
+    columnHelper = [False] * len(matrix)
+    
+    for row in range(0, len(matrix)):
+        for column in range(0, len(matrix[row])):
+            if matrix[row][column] == 0:
+                rowHelper[row] = True
+                columnHelper[column] = True
+                
+    for row in range(0, len(matrix)):
+        for column in range(0, len(matrix[row])):
+            if rowHelper[row] ^ columnHelper[column]:
+                matrix[row][column] = 0
+    return matrix
+
+if __name__ == "__main__":
+    print(clearRowColumnInMatrix([[1,2,3],[1,0,3],[3,3,3]]))
+
+                
