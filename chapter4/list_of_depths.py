2511bd1bce8ef4c13061f305664f1de3f8617cdd
CQ <chunqiu.wang@outlook.com>
2016-04-21 22:22:30 -0500

    4.3 list of depths

---
+++ chapter4/list_of_depths.py
@@ -0,0 +1,56 @@
+'''
+Created on Apr 21, 2016
+
+List of Depths: Given a binary tree, design an algorithm which creates a linked list of all 
+the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked list).
+
+@author: chunq
+'''
+
+def listOfDepths(tree):
+    result = []
+    listOfDepthsHelper(tree.root, 0, result)
+    return result
+
+def listOfDepthsHelper(node, level, result):
+    if len(result) == level:
+        result.append([])
+    result[level].append(node.key)
+    if node.left is not None:
+        listOfDepthsHelper(node.left, level + 1, result)
+    if node.right is not None:
+        listOfDepthsHelper(node.right, level + 1, result)
+
+def listOfDepthsBFS(tree):
+    result = []
+    newLevel = []
+    previousLevel = []
+    if tree.root is not None:
+        newLevel.append(tree.root)
+    while len(newLevel) > 0:
+        result.append(newLevel)
+        previousLevel = newLevel
+        newLevel = []
+        for node in previousLevel:
+            if node.left is not None:
+                newLevel.append(node.left)
+            if node.right is not None:
+                newLevel.append(node.right)
+    return result
+    
+def test():
+    from objects.BSTNode import BST
+    testTree = BST()
+    testTree.insert(5)
+    testTree.insert(3)
+    testTree.insert(7)
+    testTree.insert(1)
+    testTree.insert(4)
+    testTree.insert(9)
+    testTree.insert(8)
+    print(testTree)
+    print(listOfDepths(testTree))
+    print(listOfDepthsBFS(testTree))
+  
+if __name__ == '__main__':
+    test()
