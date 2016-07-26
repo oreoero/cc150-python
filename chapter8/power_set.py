4bab5bed71f4126337e4c8bc1e2cd3ad5e7a0431
CQ <chunqiu.wang@outlook.com>
2016-07-25 23:26:55 -0500

    added template subset unique subset

--- chapter8/power_set.py
+++ chapter8/power_set.py
@@ -18,6 +18,53 @@ def getPowerSetHelper(set):
 		list.append(set[0])
 	return subset + moreSubset
 
+def getPowerSetTemplate(set):
+	if set is None:
+		return None
+	getPowerSetTemplateHelper([], set, 0)
+	return
+
+def getPowerSetTemplateHelper(path, set, pos):
+	print(path)
+
+	for index in range(pos, len(set)):
+		path.append(set[index])
+		getPowerSetTemplateHelper(path, set, index+1)
+		path.pop(len(path)-1)
+
+	return
+
+def getUniquePowerSet(set):
+	if set is None:
+		return None
+	getUniquePowerSetHelper([], set, 0)
+	return
+
+def getUniquePowerSetHelper(path, set, pos):
+	print(path)
+
+	for index in range(pos, len(set)):
+		if (index > 0) and (set[index -1] == set[index]):
+			continue
+		path.append(set[index])
+		getUniquePowerSetHelper(path, set, index+1)
+		path.pop(len(path)-1)
+
+	return 
+
+
 if __name__ == '__main__':
 	set = [3,2]
+	#my method
 	print(getPowerSet(set))
+
+	#template method
+	print()
+	print(getPowerSetTemplate(set))
+
+	#unique power set template
+	print()
+	set = [1,1,2,2]
+	print(getUniquePowerSet(set))
+
+
